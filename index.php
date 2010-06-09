f161b3349094cdd4c9a40aa12305c7672ae49807
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-06-09 13:09:39 +0200

    - prepare for <> attack

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
 <head>
-<title>i have malware</title>
+<title>jsunpack detection</title>
 </head>
 <body>
-evil script below
+<h1>jsunpack detection</h1>
+<h2>@author krzysztof at kotowicz dot net</h2>
+<p>I will redirect to an EVIL page</p>
 <script src="js.js?<?php echo time();?>" type="text/javascript"></script>
 </body>
 </html>
--- js.js
+++ js.js
@@ -1,7 +1,9 @@
 // jsunpack detection
-// @author Krzysztof Kotowicz <krzysztof at kotowicz dot net>
+// @author Krzysztof kotowicz - krzysztof at kotowicz dot net
 fake_browser = false;
-//fake_browser = (window.open.toString().match(/print/));
+// fake_browser = (window.open.toString().match(/print/));
 if (!fake_browser) {
    window.location = 'http://localhost/~koto/malware/evil.html';
 }
+
+
